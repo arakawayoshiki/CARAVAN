65f1ef169dc7c703fa5788592feb9ed33f3dde57
arakawayoshiki <yamato4tennis@gmail.com>
2021-07-03 06:04:59 +0000

    [Finish] 3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,9 +5,10 @@ class BlogsController < ApplicationController
 
   def show
     @blog = Blog.find(params[:id])
+    
   end
 
-  def new 
+  def new
     @blog = Blog.new
   end
 
@@ -16,25 +17,25 @@ class BlogsController < ApplicationController
     blog.save
     redirect_to blog_path(blog.id)
   end
-  
+
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
-  end 
-  
+  end
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
-    redirect_to blogs_pa
-  end 
-  
+    redirect_to blogs_path
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
-  end  
+  end
 end
